a8e4f6780cda61ecda763c6768c50b4d5f34ee8e
VilhenaChen <48157929+VilhenaChen@users.noreply.github.com>
2020-08-17 16:46:10 +0100

    Added comments to the code
    Ik the repo is old, but need some coments just to understant the code

--- Countdown_SSD1306.lnk.ino
+++ Countdown_SSD1306.lnk.ino
@@ -12,7 +12,7 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-//Variables declaration Hour Minute Second
+//Variables declaration Hour(hora) Minute(minu) Second(sec)
 int hora = 53;
 int minu = 57;
 int sec = 60;
@@ -33,15 +33,16 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   do {
-    display.clearDisplay();
+    display.clearDisplay(); //Used to clear the display
+    //Text stuff, Size and Color
     display.setTextSize(2);
     display.setTextColor(WHITE);
-    display.setCursor(13,0);
+    display.setCursor(13,0); //setCursor(x,y) //Change to your needs
     display.print("Countdown");
     display.setCursor(25,15);
-    //display.print("Copinho");
     display.setCursor(13,40);
     display.setTextSize(2);
+    //This if(variable < 10) ensures that if the number is below 2 digits, it will remain centered otherwise justs prints the variable
     if( hora < 10) {
       display.print("0");
       display.setCursor(25,40);
@@ -65,7 +66,9 @@ void loop() {
     display.print(":");
     display.setCursor(70,40);
     display.print(sec);
+    //Delay for 1s = 1000ms
     delay(1000);
+    //The maths behinf this, if sec==0 then minu=minu-1 and if minu==0 then hora=hora-1, just the basics of a clock
     sec = sec - 1;
     if(sec == 0) {
       sec = 60;
@@ -75,6 +78,6 @@ void loop() {
       minu = 60;
       hora = hora - 1;
     }
-    display.display();
+    display.display(); //Display the corrent countdown
   } while (hora != 0);
 }
